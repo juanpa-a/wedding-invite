db79b737cb95184c09e31988c4dd3441f11b8a96
Juanpa <juanpa@Juanpas-MacBook-Pro.local>
2026-03-07 18:09:02 -0600

    add GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://juanpa-a.github.io',
+  base: '/wedding-invite',
   vite: {
     plugins: [tailwindcss()],
   },
